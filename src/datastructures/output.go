58ea195370683b45e88b4a23599c6d49d93a4925
Gianluca Mezzetti <mezzetti@uber.com>
2021-02-25 19:19:26 +0100

    Adds solution marshaller

---
+++ src/datastructures/output.go
@@ -0,0 +1,46 @@
+package datastructures
+
+import (
+	"strconv"
+	"strings"
+)
+
+type IntersectionID int
+type StreetID string
+
+type Solution struct {
+	Schedules map[IntersectionID]*IntersectionSchedule
+}
+
+type IntersectionSchedule struct {
+	IntersectionID IntersectionID
+	StreetSchedules map[StreetID]*StreetSchedule
+}
+
+type StreetSchedule struct {
+	StreetID StreetID
+	GreenLightDuration int
+}
+
+func MarshalToSolution(solution Solution) string {
+	var sb strings.Builder
+	sb.WriteString(strconv.Itoa(len(solution.Schedules)))
+	sb.WriteString("\n")
+	for _, intersectionSchedule := range solution.Schedules {
+		sb.WriteString(strconv.Itoa(int(intersectionSchedule.IntersectionID)))
+		sb.WriteString("\n")
+		sb.WriteString(strconv.Itoa(len(intersectionSchedule.StreetSchedules)))
+		sb.WriteString("\n")
+		first := true
+		for _, streetSchedule := range intersectionSchedule.StreetSchedules {
+			if !first {
+				sb.WriteString("\n")
+			}
+			sb.WriteString(string(streetSchedule.StreetID))
+			sb.WriteString(" ")
+			sb.WriteString(strconv.Itoa(streetSchedule.GreenLightDuration))
+			first = false
+		}
+	}
+	return sb.String()
+}
