d6a2f24dadc5128a10656d36d9ba3fdea9930c1c
Jesper Sindahl <jasn@uber.com>
2021-02-25 21:56:21 +0100

    more stuff for jfn

--- src/strategies/naivev2.go
+++ src/strategies/naivev2.go
@@ -9,10 +9,10 @@ type NaiveV2 struct {
 }
 
 func (n *NaiveV2) Apply(input *datastructures.Input) Output {
-	isHit := map[datastructures.StreetID]bool{}
+	hits := map[datastructures.StreetID]int{}
 	for _, car := range input.Cars {
 		for _, street := range car.Path {
-			isHit[street.ID] = true
+			hits[street.ID] = hits[street.ID] + 1
 		}
 	}
 	schedules := map[datastructures.IntersectionID]*datastructures.IntersectionSchedule{}
@@ -22,13 +22,17 @@ func (n *NaiveV2) Apply(input *datastructures.Input) Output {
 			StreetSchedules: map[datastructures.StreetID]*datastructures.StreetSchedule{},
 		}
 
-		streets := sortByQueue(input, intersection)
+		streets := sortByHits(input, intersection, hits)
+		sum := sumByHits(input, intersection, hits)
 		for _, street := range streets {
-			if !isHit[street.ID] {
+			if hits[street.ID] == 0 {
 				continue
 			}
 			streetID := street.ID
 			duration := 1
+			if hits[streetID]*len(streets) > sum {
+				duration = 2
+			}
 			streetSchedule := &datastructures.StreetSchedule{
 				StreetID:           streetID,
 				GreenLightDuration: duration,
@@ -82,6 +86,44 @@ func sortByQueue(input *datastructures.Input, intersection *datastructures.Inter
 	return streets
 }
 
+func sortByHits(input *datastructures.Input, intersection *datastructures.Intersection, hits map[datastructures.StreetID]int) []*datastructures.Street {
+	var streets []*datastructures.Street
+	for _, streetID := range intersection.StreetsIn {
+		streets = append(streets, input.GetStreet(streetID))
+	}
+	sort.Slice(streets, func(i, j int) bool {
+		return hits[streets[i].ID] > hits[streets[j].ID]
+	})
+
+	for i := 0; i < len(streets); i++ {
+		if hits[streets[i].ID] == 0 {
+			return streets[:i]
+		}
+	}
+	return streets
+}
+
+func sumByHits(input *datastructures.Input, intersection *datastructures.Intersection, hits map[datastructures.StreetID]int) int {
+	var streets []*datastructures.Street
+	for _, streetID := range intersection.StreetsIn {
+		streets = append(streets, input.GetStreet(streetID))
+	}
+
+	sum := 0
+	for _, street := range streets {
+		sum += hits[street.ID]
+	}
+	return sum
+}
+
 func NewNaiveV2Strategy() Strategy {
 	return &NaiveV2{}
+}
+
+func computeDuration(sum int, hits int, streets int) int {
+	duration := 1
+	for i := 1; i <= streets; i++ {
+
+	}
+	return duration
 }
